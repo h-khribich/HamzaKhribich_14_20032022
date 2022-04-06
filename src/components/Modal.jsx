04c6e3d3b997697cfcde29274cd402265540218a
Hamza KHRIBICH <hamza.khribich@gmail.com>
2022-04-06 20:15:09 +0200

    Modal implemented

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import { FaTimesCircle } from "react-icons/fa"
 import { IconContext } from "react-icons";
+import { handleModal } from '../pages/createEmployee';
 
 const Modal = () => {
   return (
-    <div className='modal'>
-      <div className='modal-message'>
-        Employee successfully created!
+    <div className='modal-container hidden'>
+      <div className='modal'>
+        <div className='modal-message'>
+          Employee successfully created!
 
-      <IconContext.Provider value={{ color: "white", style: {width: "1.4em", height: "1.4em"} }}>
-        <span className='modal-close-btn'>
-          <FaTimesCircle />
-        </span>
-      </IconContext.Provider>
+          <IconContext.Provider value={{ color: "white", style: {width: "1.4em", height: "1.4em"} }}>
+            <span className='modal-close-btn' onClick={(e) => handleModal()}>
+              <FaTimesCircle />
+            </span>
+          </IconContext.Provider>
 
+        </div>
       </div>
     </div>
   );
--- src/pages/createEmployee.jsx
+++ src/pages/createEmployee.jsx
@@ -4,24 +4,38 @@ import CreateEmployeeForm from '../components/CreateEmployeeForm';
 import Modal from '../components/Modal';
 
 // EXPLAIN FUNCTION
+export const handleModal = () => {
+    const body = document.body
+    const content = document.querySelector(".content")
+    const modalContainer = document.querySelector(".modal-container")
+
+    body.classList.toggle("no-scroll")
+    content.classList.toggle("content-blurred")
+    modalContainer.classList.toggle("hidden")
+}
+
+// EXPLAIN FUNCTION
 const saveEmployee = () => {
 
+  handleModal()
 }
 
 // EXPLAIN COMPONENT
 const createEmployee = () => {
   return (
-    <div>
-      <div className='title'>
-        <h1>HRnet</h1>
+    <div className='create-employee-page'>
+      <div className='content'>
+        <div className='title'>
+          <h1>HRnet</h1>
+        </div>
+        <div className='container'>
+          <Link to="/employee-list">View Current Employees</Link>
+          <h2>Create Employee</h2>
+          <CreateEmployeeForm />
+          <button className='modal-button' onClick={(e) => saveEmployee()}>Save</button>
+        </div>
       </div>
-      <div className='container'>
-        <Link to="/employee-list">View Current Employees</Link>
-        <h2>Create Employee</h2>
-        <CreateEmployeeForm />
-        <button onClick={saveEmployee()}>Save</button>
-        <Modal />
-      </div>
+      <Modal />
     </div>
   );
 };
