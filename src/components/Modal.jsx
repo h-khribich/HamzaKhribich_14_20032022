89909557d653e5d1c0f2c7875054acac06be3112
Hamza KHRIBICH <hamza.khribich@gmail.com>
2022-04-27 01:25:53 +0200

    Modal prop msg fixed

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -7,7 +7,6 @@ import { IconContext } from "react-icons";
  */
 export const toggleModal = () => {
   const modalContainer = document.querySelector(".modal-container")
-
   document.body.classList.toggle("no-scroll")
   modalContainer.classList.toggle("hidden")
 }
@@ -16,12 +15,12 @@ export const toggleModal = () => {
  * Modal component
  * @param {String} props - Modal message
  */
-const Modal = (props) => {
+const Modal = ({ message }) => {
   return (
     <div className='modal-container hidden'>
       <div className='modal'>
         <div className='modal-message'>
-          {props.message}
+          {message}
 
           <IconContext.Provider value={{ color: "white", style: {width: "1.4em", height: "1.4em"} }}>
             <span className='modal-close-btn' onClick={(e) => toggleModal()}>
